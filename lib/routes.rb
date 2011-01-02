6d24f6b5dce3825707bc042f793589fb5ce818c8
Pradeep Elankumaran <pradeepe@gmail.com>
2011-01-02 13:16:06 -0800

    removed unnecessary comments from copied over devise stubs

---
+++ lib/routes.rb
@@ -0,0 +1,26 @@
+module ActionDispatch::Routing
+  class Mapper
+    def typus_routes
+      scope "admin", :module => :admin, :as => "admin" do
+        match "/" => "dashboard#show", :as => "dashboard"
+        match "user_guide" => "base#user_guide"
+
+        if Typus.authentication == :session
+          resource :session, :only => [:new, :create, :destroy], :controller => :session
+          resources :account, :only => [:new, :create, :show, :forgot_password] do
+            collection { get :forgot_password }
+          end
+        end
+
+        Typus.models.map { |i| i.to_resource }.each do |resource|
+          match "#{resource}(/:action(/:id(.:format)))", :controller => resource
+        end
+
+        Typus.resources.map { |i| i.underscore }.each do |resource|
+          match "#{resource}(/:action(/:id(.:format)))", :controller => resource
+        end
+      end 
+    end
+  end
+end
+
